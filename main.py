cae9a65f98b9939030ddd2740da75d3c20bd6f8d
nabihashahhh <nabihashahh@gmail.com>
2025-02-13 22:50:56 -0500

    added initial math features

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 "print('Hello, GitHub!')" 
+
+def add_numbers(a, b):
+    return a + b
+
+def multiply_numbers(a, b):
+    return a * b
+
+
+print("Adding 2 + 3:", add_numbers(2, 3))
+print("Multiplying 2 * 3:", multiply_numbers(2, 3))
